a6cbc813c461e7261b565914f7a0e00824425639
jose <josegbluz@protonmail.com>
2024-05-11 18:05:04 -0300

    Included checkbox in the node

--- app/ui/components/ToBuyNode.tsx
+++ app/ui/components/ToBuyNode.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, TouchableOpacity, TextInput } from 'react-native';
+import BouncyCheckbox from "react-native-bouncy-checkbox";
 
 export default function ToBuyNode({ item, onComplete }): React.JSX.Element {
     return (
-        <TouchableOpacity onPress={()=> onComplete(item.key)}>
-            <View className='py-2 px-4 bg-orange-100 rounded border border-orange-500 border-dotted mt-1 '>
-                <Text className={`font-medium text-lg ${item.isCompleted ? 'line-through' : ''} `}>{item.name}</Text>
-            </View>
-        </TouchableOpacity>
+        <View className='py-2 px-4 bg-orange-100 rounded border border-orange-500 border-dotted mt-1 flex flex-row'>
+            <BouncyCheckbox isChecked={item.isCompleted}
+                onPress={() => onComplete(item.key)}>
+            </BouncyCheckbox>
+            <TextInput readOnly={item.isCompleted}
+                onChangeText={(val) => { item.name = val }}
+                className={`font-medium text-lg w-full ${item.isCompleted ? 'line-through' : ''} `}>
+                {item.name}
+            </TextInput>
+        </View>
     );
 }
